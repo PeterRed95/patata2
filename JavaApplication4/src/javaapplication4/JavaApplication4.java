53e682eda8edbdeab07eaa1d13f8775667ac80c7
PeterRed95 <3348691@alu.murciaeduca.es>
2021-03-02 14:11:29 +0100

    Prueba basura

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -16,6 +16,10 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        int i = 1;
     }
     
 }
